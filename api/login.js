d4a938fbcc8781f8890fcf62ab4a84c30f6b2a90
Yash Apte <150063566+YashM202807@users.noreply.github.com>
2025-08-03 13:02:32 +0530

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,31 @@
+const { parse } = require('url');
+
+const interns = require('./data');
+const { calculateRewards, parseBody, corsHeaders } = require('./utils');
+
+module.exports = async (req, res) => {
+  Object.entries(corsHeaders).forEach(([key, value]) => res.setHeader(key, value));
+
+  if (req.method === 'OPTIONS') {
+    res.statusCode = 204;
+    return res.end();
+  }
+
+  const { pathname } = parse(req.url, true);
+
+  if (pathname === '/api/login' && req.method === 'POST') {
+    const { username, password } = await parseBody(req);
+    const intern = interns.find(i =>
+      i.name.toLowerCase() === username?.toLowerCase() && i.password === password
+    );
+    if (!intern) {
+      res.statusCode = 401;
+      return res.end(JSON.stringify({ message: 'Invalid credentials' }));
+    }
+    const { password: _, ...internData } = intern;
+    return res.end(JSON.stringify({ ...internData, ...calculateRewards(intern.donationsRaised) }));
+  }
+
+  res.statusCode = 404;
+  res.end(JSON.stringify({ message: 'Route not found' }));
+};
